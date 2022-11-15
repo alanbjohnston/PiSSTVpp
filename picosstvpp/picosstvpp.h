888a66d6cebe07b1a55c39d405eba6f9628ecf0a
alanbjohnston <alan.b.johnston@gmail.com>
2022-11-15 17:13:29 -0500

    Update picosstvpp.h

--- picosstvpp/picosstvpp.h
+++ picosstvpp/picosstvpp.h
@@ -17,4 +17,29 @@
 #include "LittleFS.h"
 #include "hardware/pwm.h" 
 
+//uint8_t  filetype       (char *filename) ;
+void     playtone       (uint16_t tonefreq , double tonedur) ;
+void     addvisheader   (void) ;
+void     addvistrailer  (void) ;
+
+uint16_t toneval_rgb        (uint8_t colorval) ;
+uint16_t toneval_yuv        (uint8_t colorval) ;
+
+void     buildaudio_m     (double pixeltime) ;
+void     buildaudio_s     (double pixeltime) ;
+void     buildaudio_r36     (void) ;
+
+#ifdef AUDIO_AIFF
+void     writefile_aiff (void) ;
+#endif
+#ifdef AUDIO_WAV
+void     writefile_wav  (void) ;
+#endif
+
+void show_dir();
+void load_files();
+void play_pwm_file();
+
+void picosstvpp_begin(int pin);
+
 #endif
