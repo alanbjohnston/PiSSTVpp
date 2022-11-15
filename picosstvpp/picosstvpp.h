62f82032880fa05cf398796d9777170066236950
alanbjohnston <alan.b.johnston@gmail.com>
2022-11-15 17:11:34 -0500

    Create picosstvpp.h

---
+++ picosstvpp/picosstvpp.h
@@ -0,0 +1,20 @@
+#ifndef PICO_SSTV_PP_H
+
+#define PICO_SSTV_PP_H
+
+#include <Arduino.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+//#include <gd.h>
+#include <time.h>
+#include <math.h>
+//#include <tgmath.h>
+//#include <magic.h>
+//#include <unistd.h>
+#include "LittleFS.h"
+#include "hardware/pwm.h" 
+
+#endif
