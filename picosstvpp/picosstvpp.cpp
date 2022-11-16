97d53f08b9211740bbb8fcfe0c8a7a9d2b6c0532
alanbjohnston <alan.b.johnston@gmail.com>
2022-11-16 10:09:37 -0500

    240

--- picosstvpp/picosstvpp.cpp
+++ picosstvpp/picosstvpp.cpp
@@ -509,7 +509,8 @@ void buildaudio_s (double pixeltime) {
     playtone( 1200 , 9000);
 
 //    for ( y=0 ; y<256 ; y++ ) {
-    for ( y=0 ; y<100 ; y++ ) {
+//    for ( y=0 ; y<100 ; y++ ) {
+    for ( y=0 ; y<240 ; y++ ) {
         // read image data
 //	Serial.println("Starting row");    
         for ( x=0 ; x<320 ; x++ ) {
