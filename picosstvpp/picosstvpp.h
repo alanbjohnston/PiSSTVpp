1069c232d8b751d47ab642d057a1958636b41ae7
alanbjohnston <alan.b.johnston@gmail.com>
2022-12-08 15:11:03 -0500

    added long return to picosstv

--- picosstvpp/picosstvpp.h
+++ picosstvpp/picosstvpp.h
@@ -55,7 +55,7 @@ void show_dir4();
 void play_pwm_file(int dds_pwm_pin) ;
 
 void picosstvpp_begin(int pin);
-void picosstvpp();
+long picosstvpp();
 void jpeg_decode(char* filename, char* fileout, bool debug);
 void sstv_end();
 
