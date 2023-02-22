e6f546d20042525db75c6006427ead7699f405e3
alanbjohnston <alan.b.johnston@gmail.com>
2023-02-22 17:17:32 -0500

    275.2

--- picosstvpp/picosstvpp.cpp
+++ picosstvpp/picosstvpp.cpp
@@ -254,8 +254,8 @@ void picosstvpp() {
             buildaudio_s(432.0);
             break;
         case 56: //Scottie 2
-//            buildaudio_s(275.2);
-	     buildaudio_s(274.0);  // was 274.4 no stripe (vertical band), 274.7 - 1 stripe instead of 3 for 275.2, 274.9 2 stripes, 275.5 auto ML280
+            buildaudio_s(275.2);
+//	     buildaudio_s(274.0);  // was 274.4 no stripe (vertical band), 274.7 - 1 stripe instead of 3 for 275.2, 274.9 2 stripes, 275.5 auto ML280
             break;
         case 76: //Scottie DX
             buildaudio_s(1080.0);
